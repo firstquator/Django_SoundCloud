3f8396f519035ebba1def13998bbdb1b8bef9c78
LeeSangHyeon <tkdgus4693@naver.com>
2022-05-26 03:38:41 +0900

    Create Music Model

--- mangproject/mangproject/urls.py
+++ mangproject/mangproject/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
-from . import music
+import music.views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', music.views.home, name = "home"),
+    # path('', music.views.home, name = "home"),
     path('music/', include('music.urls')),
     path('user/', include('user.urls')),
 ]
--- mangproject/music/models.py
+++ mangproject/music/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class playlist(models.Model):
+  genre = models.CharField(max_length = 20)
+  name = models.CharField(max_length = 30)
+  pub_data = models.DateTimeField('data published')
+  thumbnail = models.ImageField(upload_to = 'image')
