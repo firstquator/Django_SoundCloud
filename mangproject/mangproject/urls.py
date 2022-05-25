7cb909082acd9112cbcb53f49baef69821a760dd
LeeSangHyeon <tkdgus4693@naver.com>
2022-05-26 02:27:28 +0900

    Setting Again

--- mangproject/mangproject/urls.py
+++ mangproject/mangproject/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from . import music
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(''))
+    path('', music.views.home, name = "home"),
+    path('music/', include('music.urls')),
+    path('user/', include('user.urls')),
 ]
