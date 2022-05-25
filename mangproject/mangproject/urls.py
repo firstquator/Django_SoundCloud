215bb25effebd3bcc596dde24f0dd23fa93770ca
LeeSangHyeon <tkdgus4693@naver.com>
2022-05-26 04:09:30 +0900

    Create template and make navbar HTML

--- mangproject/mangproject/urls.py
+++ mangproject/mangproject/urls.py
@@ -5,7 +5,7 @@ import music.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', music.views.home, name = "home"),
+    path('', music.views.home, name = "home"),
     path('music/', include('music.urls')),
     path('user/', include('user.urls')),
 ]
--- mangproject/music/models.py
+++ mangproject/music/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class playlist(models.Model):
+class Playlist(models.Model):
   genre = models.CharField(max_length = 20)
   name = models.CharField(max_length = 30)
   pub_data = models.DateTimeField('data published')
