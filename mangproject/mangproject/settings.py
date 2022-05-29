b1eefb858e1d049dcdd2c61796319da081e2f436
LeeSangHyeon <tkdgus4693@naver.com>
2022-05-29 15:32:16 +0900

    CSS : Navbar

--- mangproject/mangproject/settings.py
+++ mangproject/mangproject/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mangproject', 'media')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'mangproject', 'static'),
