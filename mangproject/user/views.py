c0953293669bcade6ef4288c0376b4b395b93a22
LeeSangHyeon <tkdgus4693@naver.com>
2022-05-29 16:39:57 +0900

    Create signin.html and signup.htlm

--- mangproject/user/views.py
+++ mangproject/user/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def signup(request):
+  if(request.method == "POST"):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    passwordCheck = request.POST.get('passwordCheck')
+    
+
+  return render(request, 'signup.html')
+
+def signin(request):
+
+  return render(request, 'signin.html')
+
+
