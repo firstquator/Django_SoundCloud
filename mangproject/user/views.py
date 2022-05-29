075aca37d290fae394d5b4006a4f338543385315
LeeSangHyeon <tkdgus4693@naver.com>
2022-05-29 16:47:34 +0900

    Views : signin and singup

--- mangproject/user/views.py
+++ mangproject/user/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import auth
+from django.contrib.auth.models import User
 
 # Create your views here.
 def signup(request):
@@ -6,12 +8,33 @@ def signup(request):
     username = request.POST.get('username')
     password = request.POST.get('password')
     passwordCheck = request.POST.get('passwordCheck')
-    
 
-  return render(request, 'signup.html')
+    if User.objects.filter(username = username).exists():
+      return render(request, 'signup.html', {'error' : "이미 존재하는 사용자입니다."})
+
+    if password == passwordCheck:
+      user = User.objects.create_user(
+        username = username,
+        password = passwordCheck
+      )
+    else:
+      return render(request, 'signup.html', {'error' : "비밀번호 확인이 일치하지 않습니다."})
+    auth.login(request, user)
+    return redirect('/')
+  else:
+    return render(request, 'signup.html')
 
 def signin(request):
+  if(request.method == "POST"):
+      username = request.POST.get('username')
+      password = request.POST.get('password')
 
-  return render(request, 'signin.html')
+      user = auth.authenticate(request, username = username, password = password)
 
-
+      if user is not None:
+        auth.login(request, user)
+        return redirect('/')
+      else:
+        return render(request, 'signin.html', { 'error': "사용자 이름 혹은 패스워드를 다시 확인해주세요." })
+  else:
+      return render(request, 'signup.html')
